36be9c63e7028a76aa9248225394400cc5ed358e
Hamza Mohamud <101277252+hmohamud@users.noreply.github.com>
2024-11-29 00:02:02 -0500

    merge two lists solution

---
+++ src/main/java/bptn/com/Leetcode/MergeTwoSortedLists/Solution.java
@@ -0,0 +1,44 @@
+package bptn.com.Leetcode.MergeTwoSortedLists;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
+
+class Solution {
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode l1 = new ListNode();
+		ListNode l2 = l1;
+
+		while (list1 != null && list2 != null) {
+			if (list1.val <= list2.val) {
+				l2.next = list1;
+				list1 = list1.next;
+			} else {
+				l2.next = list2;
+				list2 = list2.next;
+			}
+			l2 = l2.next;
+		}
+
+		if (list1 != null) {
+			l2.next = list1;
+		} else {
+			l2.next = list2;
+		}
+
+		return l1.next;
+	}
+}
